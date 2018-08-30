65147c7d328e93a025f709d2d4c2ce0aa5e334b7
titusefferian <kiritosato@gmail.com>
2018-08-30 17:47:12 +0700

    medical result foldering

---
+++ src/components/MedicalResult/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MedicalResult extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <td>sdfsdf</td>
+                <td>sdfsdf</td>
+                <td>sdfsdf</td>
+                <td>sdfsdf</td>
+                <td>sdfsdf</td>
+                <td>sdfsdf</td>
+
+            </React.Fragment>
+        )
+    }
+
+}
+export default MedicalResult
